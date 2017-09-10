3724630067c27cc27c0864e69e763437f0886826
Kartikay <kartikay.goyle@hotmail.com>
2017-09-10 01:37:07 -0400

    Add files via upload
    Script to download 22 days of data and save them as Csv file

--- check.py
+++ check.py
@@ -1,14 +1,14 @@
-
 import pandas_datareader.data as web
 import h5py
 import datetime
 
 stocks = ['VMW','SPLK', 'CDNS', 'GOOG', 'FB', 'NVDA', 'AAPL', 'AMZN', 'MSFT']
 
-start = datetime.datetime(2015, 1, 1)
+start = datetime.datetime(2017, 8, 9)
+print start
 end = datetime.date.today()
+print end
 
 for stock in stocks:
     df = web.DataReader(stock, "yahoo", start, end)
-    if df is not None:
-        print stock
+    df.to_csv(stock+'.csv', index=True)
